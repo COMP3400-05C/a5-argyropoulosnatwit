5db619c7491459e1d166e4ab700d16a13b435167
argyropoulosn <argyropoulosn@kali.com>
2025-11-14 18:34:39 -0500

    Completed parta and partc, tests for partb did not work.

--- partb.c
+++ partb.c
@@ -4,8 +4,54 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/wait.h>
+#include <stdlib.h>
 
 int main(int argc, char* argv[]) {
-    // TODO: Complete and document
+    if (argc != 3) {
+        printf("USAGE: partb FILEIN FILEOUT\n");
+        return 1;
+    }
+
+    int fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0770);
+    if (fd < 0) {
+        perror("open");
+        return 1;
+    }
+
+    if (dup2(fd, STDOUT_FILENO) < 0) {
+        perror("dup2");
+        close(fd);
+        return 1;
+    }
+    close(fd); 
+
+    printf("Category,Count\n");
+    fflush(stdout);
+
+    pid_t pid = fork();
+    if (pid < 0) {
+        perror("fork");
+        return 1;
+    }
+
+    if (pid == 0) {
+        char *args[] = { "./parta", argv[1], NULL };
+        execv("./parta", args);
+
+        perror("execv");
+        exit(1);
+    } else {
+        int status;
+        if (waitpid(pid, &status, 0) < 0) {
+            perror("waitpid");
+            return 1;
+        }
+
+        if (WIFEXITED(status) && WEXITSTATUS(status) != 0) {
+            fprintf(stderr, "parta exited with status %d\n", WEXITSTATUS(status));
+            return WEXITSTATUS(status);
+        }
+    }
+
     return 0;
 }
--- partc.c
+++ partc.c
@@ -4,9 +4,49 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/wait.h>
+#include <stdlib.h>
 
 int main(int argc, char* argv[]) {
-    // TODO: Complete and document
+    if (argc != 2) {
+        printf("USAGE: partc FILEIN\n");
+        return 1;
+    }
+
+    int pipefd[2];
+    if (pipe(pipefd) < 0) {
+        perror("pipe failed");
+        return 1;
+    }
+
+    pid_t pid = fork();
+    if (pid < 0) {
+        perror("fork failed");
+        return 1;
+    }
+
+
+    if (pid > 0) {
+        close(pipefd[0]);                    
+        dup2(pipefd[1], STDOUT_FILENO);        
+        close(pipefd[1]);                      
+
+        char *args[] = { "./parta", argv[1], NULL };
+        execv("./parta", args);
+
+        perror("execv parta failed");
+        exit(1);
+    }
+
+    else {
+        close(pipefd[1]);                      
+        dup2(pipefd[0], STDIN_FILENO);          
+        close(pipefd[0]);                       
+
+        execlp("sort", "sort", "-t,", "-k2", "-n", NULL);
+
+        perror("exec sort failed");
+        exit(1);
+    }
 
     return 0;
 }
